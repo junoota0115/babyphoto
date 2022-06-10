60cc5ece592428ead7a906df9a1413ff0bfb2c25
junta <79247735+junoota0115@users.noreply.github.com>
2022-06-10 12:03:18 +0900

    createアクションから保存まで

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,8 +7,14 @@ class PhotosController < ApplicationController
     @photo = Photo.new
   end
   
+def create
+  Photo.create(photo_params)
+end
 
-
+private
+def photo_params
+  params.require(:photo).permit(:name,:text,:content)
+end
 
 
 end
